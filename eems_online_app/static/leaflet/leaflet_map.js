542e1f3db478884874e7c8f6b7933afa2bc5167a
mikegough <mike.gough@consbio.org>
2016-11-30 12:10:11 -0800

    Restructure eems content directory

--- eems_online_app/static/leaflet/leaflet_map.js
+++ eems_online_app/static/leaflet/leaflet_map.js
@@ -51,7 +51,7 @@ function swapImageOverlay(layerName) {
         map.removeLayer(image_overlay);
     }
 
-    var image_overlay_url = "static/eems/data/" + eems_model_id + "/overlay/png/" + layerName + ".png";
+    var image_overlay_url = "static/eems/models/" + eems_model_id + "/overlay/png/" + layerName + ".png";
     image_overlay = L.imageOverlay(image_overlay_url, overlay_bounds);
     image_overlay.addTo(map);
     image_overlay.bringToBack();
@@ -61,6 +61,6 @@ function swapImageOverlay(layerName) {
 
 function swapLegend(layerName){
     document.getElementsByClassName('info')[0].innerHTML = "<div id='LegendHeader'>" + layerName + "</div>" +
-              "<img style='float:left' src='static/eems/data/" + eems_model_id + "/overlay/legend/" + layerName + "_legend.png'>" +
+              "<img style='float:left' src='static/eems/models/" + eems_model_id + "/overlay/legend/" + layerName + "_legend.png'>" +
               "<div class='legendLabels'>"
 }
