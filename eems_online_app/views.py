a98f838cd792f7107f3e5a37244cda882b6aa74f
mikegough <mike.gough@consbio.org>
2017-01-18 11:51:33 -0800

    Remove user database component. Move to file based system.

--- eems_online_app/views.py
+++ eems_online_app/views.py
@@ -121,58 +121,30 @@ def run_eems(request):
 
     eems_operator_changes_dict["cmds"].append({"action": "RunProg"})
 
-    print json.dumps(eems_operator_changes_dict,indent=2)
+    print "Original Model ID: ", eems_model_id
+    print "Modified Model ID: ", eems_model_modified_id
 
-    print "Model ID", eems_model_id
-    print "Modified Model ID", eems_model_modified_id
-
-    cursor = connection.cursor()
+    print "Changes: " + json.dumps(eems_operator_changes_dict,indent=2)
 
     original_mpt_file = settings.BASE_DIR + '/eems_online_app/static/eems/models/{}/eemssrc/Model.mpt'.format(eems_model_id)
 
-    # If this is the first run, make a copy of the model and store it in the user models database with a unique ID.
+    # If this is the first run, create the output directories.
     if eems_model_modified_id == '':
         eems_model_modified_id = get_random_string(length=32)
 
-        query = "SELECT NAME, MODEL FROM EEMS_ONLINE_MODELS where id = '%s'" % eems_model_id
-        cursor.execute(query)
-        for row in cursor:
-            eems_model_name = row[0]
-            eems_model = row[1]
-
-        pdata = pickle.dumps(eems_model, pickle.HIGHEST_PROTOCOL)
-
-        cursor.execute("insert into EEMS_USER_MODELS (ID, NAME, MODEL) values (%s,%s,%s)", (eems_model_modified_id, eems_model_name, sqlite3.Binary(pdata)))
         os.mkdir(settings.BASE_DIR + '/eems_online_app/static/eems/models/%s' % eems_model_modified_id)
         os.mkdir(settings.BASE_DIR + '/eems_online_app/static/eems/models/%s/data' % eems_model_modified_id)
         os.mkdir(settings.BASE_DIR + '/eems_online_app/static/eems/models/%s/eemssrc' % eems_model_modified_id)
         os.mkdir(settings.BASE_DIR + '/eems_online_app/static/eems/models/%s/histogram' % eems_model_modified_id)
         os.mkdir(settings.BASE_DIR + '/eems_online_app/static/eems/models/%s/overlay' % eems_model_modified_id)
 
-        print original_mpt_file
-        mpt_file_copy = settings.BASE_DIR + '/eems_online_app/static/eems/models/{}/eemssrc/Model.mpt'.format(eems_model_modified_id)
-        shutil.copyfile(original_mpt_file,mpt_file_copy)
-    else:
-        mpt_file_copy = settings.BASE_DIR + '/eems_online_app/static/eems/models/{}/eemssrc/Model.mpt'.format(eems_model_modified_id)
-
-    # Get the current state of the model out of the database
-    query = "SELECT MODEL FROM EEMS_USER_MODELS where ID = '%s'" % eems_model_modified_id
-
-    cursor.execute(query)
-    for row in cursor:
-        modified_eems_model = pickle.loads(str(row[0]))
+    mpt_file_copy = settings.BASE_DIR + '/eems_online_app/static/eems/models/{}/eemssrc/Model.mpt'.format(eems_model_modified_id)
+    shutil.copyfile(original_mpt_file,mpt_file_copy)
 
     output_base_dir = settings.BASE_DIR + '/eems_online_app/static/eems/models/{}/'.format(eems_model_modified_id)
 
     my_mpilot_worker = MPilotWorker()
     my_mpilot_worker.HandleRqst(eems_model_modified_id, mpt_file_copy, eems_operator_changes_dict, output_base_dir, True, False, True)
-
-    # ToDo: Apply changes in the eems_operator_changes_dict to the EEMS model stored in modified_eems_model
-    # ToDo: Run EEMS on the new model
-    # ToDo: Create PNGs stored in folder that matches the user id.
-    # ToDo: Over-write the model in the EEMS_USER_MODELS Database.
-
-    #print modified_eems_model
 
     return HttpResponse(eems_model_modified_id)
 
