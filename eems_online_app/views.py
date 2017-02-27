f2ce4c825afedd31ee5ee9fbee791efa5719ff73
mikegough <mike.gough@consbio.org>
2017-02-27 14:24:49 -0800

    Fix link button. Need to insert the EXTENT and EXTENT_GCS now.

--- eems_online_app/views.py
+++ eems_online_app/views.py
@@ -212,12 +212,13 @@ def link(request):
 
     cursor = connection.cursor()
 
-    query = "SELECT NAME, EXTENT, EPSG FROM EEMS_ONLINE_MODELS where id = '%s'" % eems_model_id
+    query = "SELECT NAME, EXTENT, EXTENT_GCS, EPSG FROM EEMS_ONLINE_MODELS where id = '%s'" % eems_model_id
     cursor.execute(query)
     for row in cursor:
         eems_model_name = row[0]
         eems_extent = str(row[1])
-        epsg = str(row[2])
+        eems_extent_gcs = str(row[2])
+        epsg = str(row[3])
 
     eems_model_name_user = eems_model_name.replace(" (Modified)", "") + " (Modified)"
     user = "USER"
@@ -226,7 +227,7 @@ def link(request):
     long_description = "This model is a user modified version of the original " + eems_model_name + " model, created on " + time.strftime("%d/%m/%Y") + " at " + time.strftime("%H:%M") + ". To access the original model, click the link below.<p><a title='click to access the original model' href=?model=" + str(eems_model_id) + ">" + eems_model_name + "</a>"
     todays_date = time.strftime("%d/%m/%Y")
 
-    cursor.execute("insert into EEMS_ONLINE_MODELS (ID, NAME, EXTENT, OWNER, SHORT_DESCRIPTION, LONG_DESCRIPTION, AUTHOR, CREATION_DATE, EPSG)  values (%s,%s,%s,%s,%s,%s,%s,%s,%s)", (eems_model_modified_id, eems_model_name_user, eems_extent, user, short_description, long_description, author, todays_date, epsg))
+    cursor.execute("insert into EEMS_ONLINE_MODELS (ID, NAME, EXTENT, EXTENT_GCS, OWNER, SHORT_DESCRIPTION, LONG_DESCRIPTION, AUTHOR, CREATION_DATE, EPSG)  values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (eems_model_modified_id, eems_model_name_user, eems_extent, eems_extent_gcs, user, short_description, long_description, author, todays_date, epsg))
 
     return HttpResponse(eems_model_modified_id)
 
