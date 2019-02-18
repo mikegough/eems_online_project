8537f7f3ff504a5a7b5b57317ec16e23e46dfe50
mikegough <mike.gough@consbio.org>
2019-02-18 15:57:11 -0800

    Hack to fix trouble with displahying project description first

--- eems_online_app/static/js/ssn.js
+++ eems_online_app/static/js/ssn.js
@@ -1,6 +1,7 @@
 // Initialize EEMS model if first time clicking on the model tab.
 JitInitializationComplete=true
 var click_once = false;
+$("#home").hide();
 $("#home_link").on("click", function(){
     if (! click_once) {
         $.getJSON(init_eems_file, function (results) {
