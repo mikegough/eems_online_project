62a56fba6ad062f55886dc2ecbbc3c0b52b5a952
mikegough <mike.gough@consbio.org>
2019-07-04 15:10:36 -0700

    CNPS initialize

---
+++ eems_online_app/static/js/cnps.js
@@ -0,0 +1,14 @@
+// Initialize EEMS model if first time clicking on the model tab.
+var click_once = false;
+$("#home_link").on("click", function(){
+    JitInitializationComplete=false;
+    if (! click_once) {
+        $.getJSON(init_eems_file, function (results) {
+            json = results;
+            init(json, init_eems_file_name);
+        });
+    }
+    click_once = true;
+});
+
+
