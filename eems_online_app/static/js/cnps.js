22c01abd1dd3cdb95aad611a4c2fc291716a0640
mikegough <mike.gough@consbio.org>
2019-08-09 11:41:15 -0700

    login & link controls

--- eems_online_app/static/js/cnps.js
+++ eems_online_app/static/js/cnps.js
@@ -1,3 +1,18 @@
+// Only show the Download Button for approved users.
+$( document ).ready(function() {
+    if ((["sam.young", "mike.gough"]).indexOf(eems_user) == -1) {
+        $("#download_label").addClass("permanently_disabled");
+    }
+});
+
+// If a user accesses the model through a model link
+if (initial_tab == "model"){
+    $("#home_link").removeClass("active");
+    $("#model_link").addClass("active");
+    $("#model").show();
+    $("#home").hide();
+}
+
 // Initialize EEMS model if first time clicking on the model tab.
 var click_once = false;
 $("#model_link").on("click", function(){
