210f9e8e6bb0748940ef28cfa7c603b23e02e623
mikegough <mike.gough@consbio.org>
2021-02-19 07:50:09 -0800

    Update change_path.py

--- scripts/change_path.py
+++ scripts/change_path.py
@@ -2,15 +2,7 @@ import os
 #search = "/home/consbio/webapps/eems/" 
 #replace =  "/home/mike/apps/eems-online/"
 search =  "/home/mike/apps/eems-online/"
-search =  "../../"
-search =  "../../../"
-search =  "../../../../"
-search =  "./eems/eems"
-search =  ".../eems"
-search =  "../eems"
-search =  "../../eems"
-search =  "/home/consbio/webapps/eems/"
-replace =  "/home/mike/apps/eems-online/"
+replace = "F:/Projects2/EEMS_Online/tasks/web_applications/eems_online_django/"
 for dname, dirs, files in os.walk("./"):
     for fname in files:
         fpath = os.path.join(dname, fname)
